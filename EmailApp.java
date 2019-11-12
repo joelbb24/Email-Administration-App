28c82b1db0310fbb908efa8d13fec52cfc6d4e1e
Joel Raj <joelanilraj2410@gmail.com>
2019-11-12 11:06:51 +0530

    Create EmailApp.java

---
+++ EmailApp.java
@@ -0,0 +1,18 @@
+package emailapp;
+
+import java.util.Scanner;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first name : ");
+		String firstName = sc.nextLine();
+		System.out.println("Enter Last Name : ");
+		String lastName = sc.nextLine();
+		Email em1 = new Email(firstName,lastName);
+		
+		System.out.println(em1.showInfo());
+		sc.close();
+	}
+}
